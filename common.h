7d31d8f181d5989f8f11634951695f96e4a9cbf6
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-03-20 15:01:32 -0400

    Removed tempId

--- common.h
+++ common.h
@@ -15,6 +15,5 @@ extern float temperature; /**<The temperature variable*/
 extern float accCorrectedValues[3]; /**<The corrected accelerometer values*/
 extern float angles[2]; /**<A variable containing the pitch and roll */
 
-extern osSemaphoreId tempId;
 extern osSemaphoreId accId;
 #endif
