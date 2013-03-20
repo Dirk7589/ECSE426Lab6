b7175406592aadb70fee160311f0dc43310fffd4
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-03-20 15:05:46 -0400

    Added new wireless module

---
+++ wireless.c
@@ -0,0 +1,10 @@
+/**
+*@file wireless.c
+*@author Dirk Dubois, Alain Slak
+*@date March 20th, 2013
+*@brief A set of functions to interface with the ez430-CC2500
+*
+*/
+
+/*Includes*/
+#include "wireless.h"
---
+++ wireless.h
@@ -0,0 +1,13 @@
+/**
+*@file wireless.h
+*@author Dirk Dubois, Alain Slak
+*@date March 20th, 2013
+*@brief A set of functions to interface with the ez430-CC2500
+*
+*/
+
+#ifndef __WIRELESS_H
+#define __WIRELESS_H
+
+
+#endif
