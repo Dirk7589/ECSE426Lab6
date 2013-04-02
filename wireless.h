fdfc9a831eabc86967f2ddc31a83f63baa480022
jason-cohen <jason.cohen@mail.mcgill.ca>
2013-04-02 12:25:49 -0400

    SMARTRF settings configured for wireless communication. Important configuration values are for ADDR, CHNNR, PKTCTRL1, PKTCTRL0, SYNC1, SYNC0. ADDR is used for packet filtering. SYNCx is used for packet filtering as well, but in a different manner. It is prepended to the packet and is used to determine if a valid packet has arrived on the RX side. PKTCTRLx controls whether CRC is used, if upon CRC failure (automated) the RX FIFO is flushed, if there are 2 appended bytes to the packet with validation information, and if addressing will be compared against ADDR and configuration of the packet length (variable, fixed).

--- wireless.h
+++ wireless.h
@@ -9,17 +9,22 @@
 #ifndef __WIRELESS_H
 #define __WIRELESS_H
 
+#define WIRELESS_CS_LOW()       			  	 GPIO_ResetBits(WIRELESS_CS_PORT, WIRELESS_CS_PIN)
+#define WIRELESS_CS_HIGH()      					 GPIO_SetBits(WIRELESS_CS_PORT, WIRELESS_CS_PIN)
+
+#define WIRELESS_FLAG_TIMEOUT         ((uint32_t)0x1000)
+
 //defines
 #define SMARTRF_SETTING_IOCFG2 0x29
 #define SMARTRF_SETTING_IOCFG1 0x2E
 #define SMARTRF_SETTING_IOCFG0 0x06
 #define SMARTRF_SETTING_FIFOTHR 0x07
-#define SMARTRF_SETTING_SYNC1 0x00
-#define SMARTRF_SETTING_SYNC0 0xFF
-#define SMARTRF_SETTING_PKTLEN 0x06	//used to be A
-#define SMARTRF_SETTING_PKTCTRL1 0x04
-#define SMARTRF_SETTING_PKTCTRL0 0x05
-#define SMARTRF_SETTING_ADDR 0x00
+#define SMARTRF_SETTING_SYNC1 0xA3
+#define SMARTRF_SETTING_SYNC0 0xD9
+#define SMARTRF_SETTING_PKTLEN 0x03	//used to be A
+#define SMARTRF_SETTING_PKTCTRL1 0x0E // Changed so that there is an appended status to the payload and address checking based on the ADDR value with broadcasting
+#define SMARTRF_SETTING_PKTCTRL0 0x05 // CRC enabled and variable packet length enabled, so packet length must be defined as the first byte of the payload (payload length doesn't include this byte)
+#define SMARTRF_SETTING_ADDR 0x10
 #define SMARTRF_SETTING_CHANNR 0x08 //Set the channel
 #define SMARTRF_SETTING_FSCTRL1 0x0C
 #define SMARTRF_SETTING_FSCTRL0 0x00
@@ -31,14 +36,14 @@
 #define SMARTRF_SETTING_MDMCFG2 0x73 
 #define SMARTRF_SETTING_MDMCFG1 0x42
 #define SMARTRF_SETTING_MDMCFG0 0xF8
-#define SMARTRF_SETTING_DEVIATN 0x01 //CHANGED
+#define SMARTRF_SETTING_DEVIATN 0x00 // ***
 #define SMARTRF_SETTING_MCSM2 0x07
 #define SMARTRF_SETTING_MCSM1 0x30
 #define SMARTRF_SETTING_MCSM0 0x18
 #define SMARTRF_SETTING_FOCCFG 0x1D
 #define SMARTRF_SETTING_BSCFG 0x1C
 #define SMARTRF_SETTING_AGCCTRL2 0xC7
-#define SMARTRF_SETTING_AGCCTRL1 0x00 //CHANGED
+#define SMARTRF_SETTING_AGCCTRL1 0x00 // ***
 #define SMARTRF_SETTING_AGCCTRL0 0xB0
 #define SMARTRF_SETTING_WOREVT1 0x87
 #define SMARTRF_SETTING_WOREVT0 0x6B
@@ -58,8 +63,14 @@
 #define SMARTRF_SETTING_TEST1 0x31
 #define SMARTRF_SETTING_TEST0 0x0B
 
-/********************************/
+/*Status registers*/
 #define PARTNUM 0x30
+#define VERSION 0x31
+#define MARCSTATE 0x35 /**<Main Radio Control State Machine State*/
+#define TXBYTES 0x3A /**<Underflow and number of bytes in the TX FIFO (needs to be ORed with MULTIPLEBYTE_RD)*/
+#define RXBYTES 0x3B /**<Overflow and number of bytes in the RX FIFO (needs to be ORed with MULTIPLEBYTE_RD)*/
+#define PKTSTATUS 0x38 /**<Current GDOx Status and Packet Status*/
+
 #define RXFIFO_SINGLE 0xBF
 #define RXFIFO_BURST 0xFF
 #define TXFIFO_SINGLE 0x3F
@@ -72,15 +83,21 @@
 #define TX_UNDERFLOW 0x70
 
 /*Command strobes*/
+#define SRES 0x30 /**<Reset chip*/
+#define SFXTXON 0x31 /**<Enable and calibrate frequency synthesizer*/
+#define SXOFF 0x32 /**<Turn off crystal oscillator*/
+#define SCAL 0x33 /**<Calibrate frequency synthesizer and turn it off*/
 #define SRX 0x34 /**<Enter receive mode*/
-#define STX 0x35 /**,Enter transmit mode*/
+#define STX 0x35 /**<Enter transmit mode*/
+#define SIDLE 0x36 /**<Exit RX / TX, turn off frequency synthesizer and exit Wake-On-Radio mode if applicable*/
+#define SWOR 0x38 /**<Start automatic RX polling sequence*/
+#define SPWD 0x39 /**<Enter power down mode when CSn goes high*/
 #define SFRX 0x3A /**<The strobe command to flush the RX FIFO*/
 #define SFTX 0x3B /**<The strobe command to flush the TX FIFO*/
-#define IDLE 0x36 /**<The strobe command to idle the state machin*/
 
 /*Wireless SPI defines*/
-#define WIRELESS_BUFFER_SIZE 6
-#define WIRELESS_BUFFER_INIT_SIZE 48
+#define WIRELESS_BUFFER_SIZE 5
+#define WIRELESS_BUFFER_INIT_SIZE 47
 #define WIRELESS_CS_PIN 0x0010 /**<Select pin4 for wireless SPI1 CSn*/
 #define WIRELESS_CS_PORT GPIOA /**<Select portA for wireless SPI CSn*/
 
@@ -100,27 +117,47 @@ void initWireless(void);
 
 /**
 *@brief A function to write to a specified register on the wireless board
-*@param[in] data The data to be sent to the device
-*@param[in] address The address of the register to send data to
+*@param[in] pBuffer The data to be sent to the device
+*@param[in] writeAddr The address of the register to send data to
 *@param[in] numOfBytes The number of bytes to be sent
 *@retval None
 */
-void wirelessWrite(uint8_t* data, uint8_t address, uint16_t numOfBytes);
+void wirelessSend(uint8_t* pBuffer, uint8_t writeAddr, uint16_t numOfBytes);
 
 /**
 *@brief A function that writes a byte for the Wireless module over SPI
 *@param[in] data The data to be written to the wireless device
-*@retval None
+*@retval uint8_t
 */
-void wirelessSendByte(uint8_t data);
+uint8_t wirelessSendByte(uint8_t data);
 
 /**
 *@brief A function to read from a specified resgister on the wireless board
-*@param[inout] data The data to be read to the device
-*@param[in] address The address of the register to read data to
+*@param[inout] pBuffer The data to be read to the device
+*@param[in] readAddr The address of the register to read data to
 *@param[in] numOfBytes The number of bytes to be sent
 *@retval None
 */
-void wirelessRead(uint8_t* data, uint8_t address, uint16_t numOfBytes);
+void wirelessRead(uint8_t* pBuffer, uint8_t readAddr, uint16_t numOfBytes);
+
+/**
+  * @brief  Send packets wirelessly
+  * @param  pitchBuffer - array holding values for the pitch angle
+	*					rollBuffer 	- array holding values for the roll angle
+	*					buffLength	- length of the two above arrays
+  * @retval None
+  */
+
+void wirelessTX(uint8_t byte);
+
+/**
+  * @brief  Receive packets wirelessly
+  * @param  pitchBuffer - array holding values for the pitch angle
+	*					rollBuffer 	- array holding values for the roll angle
+	*					buffLength	- length of the two above arrays
+	*					packet			- received packet, returned by reference
+  * @retval None
+  */
+void wirelessRX(uint8_t* packet);
 
 #endif
